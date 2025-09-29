207b0fe5cfc9e102a6f0db259ccdd853bae0531c
TanmayBhapkar <bhapkartanmay88@gmail.com>
2025-09-29 23:45:28 +0530

    Delete mern-excel-project directory

--- mern-excel-project/backend/src/middleware/checkRole.js
+++
@@ -1,9 +0,0 @@
-export default function(roles = []) {
-  return (req, res, next) => {
-    if (!req.user) return res.status(401).json({ error: 'Unauthorized' });
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ error: 'Forbidden: insufficient role' });
-    }
-    next();
-  };
-}
